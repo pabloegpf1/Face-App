31f6e5527498e6629b81ee39fe537ed42a111009
pabloegpf1 <pabloescrivagallardo@gmail.com>
2021-01-07 20:15:22 +0100

    Added server semaphore to improve fps

--- src/scripts/faceApiServer.js
+++ src/scripts/faceApiServer.js
@@ -2,6 +2,8 @@ import socketIOClient from "socket.io-client";
 import * as constants from '../constants';
 import * as utils from './utils';
 
+let waitingForFrame = false;
+
 let socket = socketIOClient(
     process.env.NODE_ENV === "production" ?
     constants.SERVER_URL : constants.SERVER_URL_DEV
@@ -14,7 +16,9 @@ socket.on('connect', function() {
 
 export const recognize = (base64image, updateTimeStats) => {
     const initialTime = Math.round(performance.now());
+    waitingForFrame = true;
     socket.emit("recognize", {base64image, initialTime}, async (response) => {
+        waitingForFrame = false;
         if(response.success) {
             const canvas = document.createElement('canvas');
             const image = await utils.createImageFromBase64(response.base64canvas);
